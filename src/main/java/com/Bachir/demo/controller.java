63a4afadfde774affb2f9e07cbb45255b772f106
Ansar Bachir <ansar.bachir.94@gmail.com>
2024-09-24 13:19:46 +0100

    change api version to 2

--- src/main/java/com/Bachir/demo/controller.java
+++ src/main/java/com/Bachir/demo/controller.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author HP
  */
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v2/")
 public class controller {
     
     
